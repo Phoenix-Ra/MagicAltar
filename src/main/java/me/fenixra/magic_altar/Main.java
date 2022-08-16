5b1f2e01054c1262f74c6cf58cae94c79e0ea197
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2022-08-16 14:23:17 +0200

    feat: metrics and version checker

--- src/main/java/me/fenixra/magic_altar/Main.java
+++ src/main/java/me/fenixra/magic_altar/Main.java
@@ -3,7 +3,15 @@ package me.fenixra.magic_altar;
 import me.fenixra.magic_altar.files.ConfigFile;
 import me.fenixra.magic_altar.files.DataFile;
 import me.fenixra.magic_altar.utils.FenixFileManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main extends JavaPlugin {
     private static Main instance;
@@ -19,19 +27,58 @@ public class Main extends JavaPlugin {
         fileManager=new FenixFileManager(Main.getInstance());
         fileManager.addFile(new ConfigFile(fileManager)).addFile(new DataFile(fileManager));
         fileManager.loadfiles();
+        try {
+            if ((new Metrics(this, 16158)).isEnabled()) {
+                Bukkit.getConsoleSender().sendMessage("§7Metrics loaded successfully");
+            }
+            doAsync(this::checkVersion);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-
-    public void reload(){
-        fileManager.reloadFiles();
-    }
-
 
     @Override
     public void onDisable() {
         altarM.clearAll(true);
 
     }
+    public void reload(){
+        fileManager.reloadFiles();
+    }
 
+    private void checkVersion() {
+        String currentVersion = Main.getInstance().getDescription().getVersion();
+        URL url;
+        try {
+            url = new URL("https://api.spigotmc.org/legacy/update.php?resource=104593");
+        }
+        catch (MalformedURLException e) {
+            return;
+        }
+        URLConnection conn;
+        try {
+            conn = url.openConnection();
+        }
+        catch (IOException e) {
+            return;
+        }
+        try {
+            assert (conn != null);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String newVersion=reader.readLine();
+            if (!newVersion.equals(currentVersion)) {
+                Bukkit.getConsoleSender().sendMessage("§6A new version available! Download §MagicAltar v"
+                        +newVersion+" §6at https://www.spigotmc.org/resources/104593/");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void doSync(Runnable runnable) {
+        instance.getServer().getScheduler().runTask(instance, runnable);
+    }
+    public static void doAsync(Runnable runnable) {instance.getServer().getScheduler().runTaskAsynchronously(instance, runnable); }
     public static Main getInstance() {
         return instance;
     }
